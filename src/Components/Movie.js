7fc3b627df3e00f47bc7eb1c3ce3f454bfc7aeda
Ghazanfar Ijaz <95541056+MGhazanfarijaz@users.noreply.github.com>
2023-06-06 01:51:48 +0500

    after17

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -9,7 +9,7 @@ function Movie({movie}) {
         <Link to={`/movies/${movie?.name}`} className='w-full'>
             <img src={`${movie?.image}`} alt={movie?.name} className='w-full h-64 object-cover' />
         </Link>
-        <div className='absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3'>
+        <div className='absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-black bg-opacity-60 text-white px-4 py-3'>
         </div>
      </div>
    </>
