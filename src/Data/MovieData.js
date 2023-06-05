0916c08c1c614cab0b9a0af4175a9c037b7d4476
MuhammadAhmed-1855 <i201855@nu.edu.pk>
2023-06-05 22:27:36 +0500

    Additional Details

---
+++ src/Data/MovieData.js
@@ -0,0 +1,10 @@
+export const Movies = [
+    {
+        name: 'Army',
+        desc: "Whatever",
+        titleImage: "logo.png",
+        category: "western",
+        rate: "3.4",
+        reviews: 23
+    }
+];
