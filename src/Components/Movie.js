5d31614462c06413c3e8d6661d6ef090b96820bc
Ghazanfar Ijaz <95541056+MGhazanfarijaz@users.noreply.github.com>
2023-06-06 01:50:56 +0500

    Your commit message

---
+++ src/Components/Movie.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Movie({movie}) {
+  return (
+   <>
+     <div className='border border-border p-1 hover:scale-95 transitions relative rounded overflow-hidden'>
+        <Link to={`/movies/${movie?.name}`} className='w-full'>
+            <img src={`${movie?.image}`} alt={movie?.name} className='w-full h-64 object-cover' />
+        </Link>
+        <div className='absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3'>
+        </div>
+     </div>
+   </>
+  )
+}
+
+export default Movie
---
+++ src/Data/MovieData.js
@@ -0,0 +1,43 @@
+export const Movies = [
+    {
+        name: 'I AM VENGENCE',
+        desc: "Whatever",
+        titleImage: "1.png",
+        image: 'https://c4.wallpaperflare.com/wallpaper/808/255/832/the-batman-2022-robert-pattinson-bruce-wayne-batman-logo-dark-hd-wallpaper-preview.jpg',
+        titla: '1.jpg',
+        category: "western",
+        year: '2023',
+        time: '3hr',
+        rate: "3.4",
+        reviews: 23
+    },
+
+
+    {
+        name: 'SpoderMAN',
+        desc: "Whatever",
+        titleImage: "1.png",
+        image: 'https://wallpaperfx.com/view_image/the-amazing-spiderman-2-1920x1200-wallpaper-12738.jpg',
+        titla: '1.jpg',
+        category: "western",
+        year: '2014',
+        time: '2.5hr',
+        rate: "3.4",
+        reviews: 23
+    },
+
+    {
+        name: 'I AM MADRISTA',
+        desc: "Whatever",
+        titleImage: "1.png",
+        image: 'https://wallpapercrafter.com/desktop1/660617-football-real-madrid-sergio-ramos-soccer-karim.jpg',
+        titla: '1.jpg',
+        category: "western2",
+        year: '2013',
+        time: '2.5hr',
+        rate: "3.4",
+        reviews: 23
+    }
+];
+
+export default Movies;
